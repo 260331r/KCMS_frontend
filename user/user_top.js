8dfe39b5343ce8c97355b62f17bd0a65e16b5e07
Shun260334 <260334k@ugs.kochi-tech.ac.jp>
2024-12-31 15:31:49 +0900

    購入者トップページ修正

--- user/user_top.js
+++ user/user_top.js
@@ -1,7 +1,11 @@
 document.querySelector("#NEXT_BUTTON").addEventListener("click", function() {
     const selectElement = document.getElementById("SEARCH_TYPE_BOX");
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
     const selectedValue = selectElement.value;
 
+    if (selectedOption.hidden) {
+        preventDefault();
+    }
     if (selectedValue) {
         window.location.href = selectedValue;
     }
