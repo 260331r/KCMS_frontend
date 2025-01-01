0b6441a902f5c2bbab909d04961d02b5f4e914c4
260331r <147483520+260331r@users.noreply.github.com>
2025-01-01 20:50:34 +0900

    Update waiting_list.js

--- place/waiting_list.js
+++ place/waiting_list.js
@@ -1,20 +1,29 @@
 // 初期設定
 document.addEventListener("DOMContentLoaded", () => {
-    init_create_schedule_list();
+    init_create_kitchen_list();
+    give_function_search_button();
 });
 
-// 本日以降のすべての日時のリストを作成する関数
-function init_create_schedule_list(){
-    const user_id = localStorage.getItem("user_id");
-    const all_elements = db_all_elements(user_id);
+// すべての日時のリストを作成する関数
+function init_create_kitchen_list(){
+    const all_elements = db_all_elements();
     const list = document.getElementById("list");
-
+    clear_error_text();
     create_kitchen_frame_list(all_elements, list);
 
     return 0;
 }
 
-// リストの枠を量産する関数
+// 与えられた日時情報で絞ってリストを作成する
+function create_kitchen_list(date, list){
+    const searched_elements = db_search_elements(date);
+    clear_error_text();
+    create_kitchen_frame_list(searched_elements, list);
+    
+    return 0;
+}
+
+// リストに表示する枠を作成する
 function create_kitchen_frame_list(shop_array, list){
     for(let i = 0; i < shop_array.length; i++){
         const frame = make_frame(shop_array[i]);
@@ -24,14 +33,13 @@ function create_kitchen_frame_list(shop_array, list){
     return 0;
 }
 
-// リストに表示する枠を作成する
 function make_frame(shop_details){
     const frame = document.createElement("div");
     frame.className = "FRAME_DESIGN";
 
     const shop_schedule = document.createElement("p");
     shop_schedule.className = "SCHEDULE_TEXT";
-    shop_schedule.textContent = "出店予定日時 : " + shop_details.日時;
+    shop_schedule.textContent = "出店希望日時 : " + shop_details.日時;
     frame.append(shop_schedule);
 
     const shop_name = document.createElement("p");
@@ -44,18 +52,44 @@ function make_frame(shop_details){
     shop_genre.textContent = "商品ジャンル : " + shop_details.商品ジャンル;
     frame.append(shop_genre);
 
+    frame.addEventListener("click", () => {
+        // ここで，出店情報の詳細を表示するページに飛ばす(この時，キッチンカーのIDを持たせて遷移させる)
+    });
+
     return frame;
 }
 
-// データベースから本日以降のすべての提供予定の要素を取得する関数
-async function db_all_elements(user_id){
-    const response = await fetch("http://127.0.0.1:8000/api/locate/place_owner_schedule_check", {
+// データベースからすべてのマッチング待ちキッチンカーの要素を取得する関数
+async function db_all_elements(){
+    const response = await fetch("http://127.0.0.1:8000/api/locate/place_owner_matching_request", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    try {
+        const result = await response.json();
+
+        if (!response.ok || !result || result.length <= 0) {
+            create_not_search_text();
+        }
+    } catch (error) {
+        create_server_error_text();
+    }
+
+    return result;
+}
+
+// データベースから条件で絞った要素を取得する関数
+async function db_search_elements(date){
+    const response = await fetch("http://127.0.0.1:8000/api/locate/place_owner_matching_request", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            "user_id": user_id
+            "date": date
         })
     });
 
@@ -70,6 +104,36 @@ async function db_all_elements(user_id){
     }
 
     return result;
+}
+
+// 検索ボタンに検索機能を与える関数
+function give_function_search_button(){
+    const search_button = document.getElementById("search_button");
+    search_button.addEventListener("click", () => {
+        search_by_date();
+    });
+
+    return 0;
+}
+
+// 検索処理を行う機能
+function search_by_date(){
+    const date = document.getElementById("date").value;
+    if(date == "") return;
+    const list = document.getElementById("list");
+    list.innerHTML = "";
+
+    create_kitchen_list(date, list);
+
+    return 0;
+}
+
+// エラー文をクリアする関数
+function clear_error_text(){
+    const text_box = document.getElementById("error");
+    text_box.innerHTML = "";
+
+    return 0;
 }
 
 // 検索結果が見つからなかった時のエラー文を表示する関数
