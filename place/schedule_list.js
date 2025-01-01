1e04733ff048c73ee384746d5c1194d9eac2f1c6
260331r <147483520+260331r@users.noreply.github.com>
2025-01-01 20:48:40 +0900

    Update schedule_list.js

--- place/schedule_list.js
+++ place/schedule_list.js
@@ -5,18 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // 本日以降のすべての日時のリストを作成する関数
 function init_create_schedule_list(){
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-
-    const all_elements = db_all_elements(year, month, day);
+    const user_id = localStorage.getItem("user_id");
+    const all_elements = db_all_elements(user_id);
     const list = document.getElementById("list");
 
     create_kitchen_frame_list(all_elements, list);
-    if(all_elements.length <= 0){
-        create_error_text();
-    }
 
     return 0;
 }
@@ -38,35 +31,59 @@ function make_frame(shop_details){
 
     const shop_schedule = document.createElement("p");
     shop_schedule.className = "SCHEDULE_TEXT";
-    shop_schedule.textContent = "出店予定日時 : " + shop_details[2];
+    shop_schedule.textContent = "出店予定日時 : " + shop_details.日時;
     frame.append(shop_schedule);
 
     const shop_name = document.createElement("p");
     shop_name.className = "NAME_TEXT";
-    shop_name.textContent = shop_details[0];
+    shop_name.textContent = shop_details.出店者名;
     frame.append(shop_name);
 
     const shop_genre = document.createElement("p");
     shop_genre.className = "GENRE_TEXT";
-    shop_genre.textContent = "商品ジャンル : " + shop_details[1];
+    shop_genre.textContent = "商品ジャンル : " + shop_details.商品ジャンル;
     frame.append(shop_genre);
 
     return frame;
 }
 
 // データベースから本日以降のすべての提供予定の要素を取得する関数
-// [[店名, ジャンル, 掲示日時], ...]のような形式
-// スケジュールテーブルから、日時がnullでないものかつ、場所提供者IDが自身と一致しているものを取り出す
-function db_all_elements(year, month, day){
-    // テストのため適当に配列を作っているだけ
-    return [[2, 3, 4], [5,5,5]];
+async function db_all_elements(user_id){
+    const response = await fetch("http://127.0.0.1:8000/api/locate/place_owner_schedule_check", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "user_id": user_id
+        })
+    });
+
+    try {
+        const result = await response.json();
+
+        if (!response.ok || !result || result.length <= 0) {
+            create_not_search_text();
+        }
+    } catch (error) {
+        create_server_error_text();
+    }
+
+    return result;
 }
 
-// エラー文を表示する関数
-function create_error_text(){
+// 検索結果が見つからなかった時のエラー文を表示する関数
+function create_not_search_text(){
     const text_box = document.getElementById("error");
-    text_box.className = "COMMON_TEXT";
     text_box.textContent = "検索結果が見つかりませんでした";
+
+    return 0;
+}
+
+// データベースとの通信でエラーが起こった場合のエラー文を表示する関数
+function create_server_error_text(){
+    const text_box = document.getElementById("error");
+    text_box.textContent = "サーバーエラーが起こりました";
 
     return 0;
 }
