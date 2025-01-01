e45d66607d43068e0a7da81ed92f66fcbd6b7477
260331r <147483520+260331r@users.noreply.github.com>
2025-01-01 17:05:23 +0900

    Update waiting_list.js

--- place/waiting_list.js
+++ place/waiting_list.js
@@ -1,8 +1,6 @@
 // 初期設定
 document.addEventListener("DOMContentLoaded", () => {
     init_create_kitchen_list();
-    set_months();
-    set_days();
     give_function_search_button();
 });
 
@@ -10,24 +8,18 @@ document.addEventListener("DOMContentLoaded", () => {
 function init_create_kitchen_list(){
     const all_elements = db_all_elements();
     const list = document.getElementById("list");
-
+    clear_error_text();
     create_kitchen_frame_list(all_elements, list);
-    if(searched_elements.length <= 0){
-        create_error_text();
-    }
+
     return 0;
 }
 
 // 与えられた日時情報で絞ってリストを作成する
-function create_kitchen_list(year, month, day, list){
-    const searched_elements = db_search_elements(year, month, day);
-
-    create_kitchen_frame_list(searched_elements, list);
+function create_kitchen_list(date, list){
+    const searched_elements = db_search_elements(date);
     clear_error_text();
-    if(searched_elements.length <= 0){
-        create_error_text();
-    }
-
+    create_kitchen_frame_list(searched_elements, list);
+    
     return 0;
 }
 
@@ -47,67 +39,71 @@ function make_frame(shop_details){
 
     const shop_schedule = document.createElement("p");
     shop_schedule.className = "SCHEDULE_TEXT";
-    shop_schedule.textContent = "出店希望日時 : " + shop_details[3];
+    shop_schedule.textContent = "出店希望日時 : " + shop_details.日時;
     frame.append(shop_schedule);
 
     const shop_name = document.createElement("p");
     shop_name.className = "NAME_TEXT";
-    shop_name.textContent = shop_details[1];
+    shop_name.textContent = shop_details.出店者名;
     frame.append(shop_name);
 
     const shop_genre = document.createElement("p");
     shop_genre.className = "GENRE_TEXT";
-    shop_genre.textContent = "商品ジャンル : " + shop_details[2];
+    shop_genre.textContent = "商品ジャンル : " + shop_details.商品ジャンル;
     frame.append(shop_genre);
 
     frame.addEventListener("click", () => {
-        // ここで，出店情報の詳細を表示するページに飛ばす
+        // ここで，出店情報の詳細を表示するページに飛ばす(この時，キッチンカーのIDを持たせて遷移させる)
     });
 
     return frame;
 }
 
 // データベースからすべてのマッチング待ちキッチンカーの要素を取得する関数
-// [[id, 店名, ジャンル, 掲示日時], ...]のような形式
-function db_all_elements(){
-    // テストのため適当に配列を作っているだけ
-    return [[1, 2, 3, 4], [5,5,5,5]]
+async function db_all_elements(){
+    const response = await fetch("http://127.0.0.1:8000/api/locate/place_owner_matching_request", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    try {
+        const result = await response.json();
+
+        if (!response.ok || !result || result.length <= 0) {
+            create_not_search_text();
+        }
+    } catch (error) {
+        create_server_error_text();
+    }
+
+    return result;
 }
 
 // データベースから条件で絞った要素を取得する関数
-function db_search_elements(year, month, day){
-    // テスト用の配列
-    return [[1, 2, 3, 4], [4, 3,2 ,12]]
-}
+async function db_search_elements(date){
+    const response = await fetch("http://127.0.0.1:8000/api/locate/place_owner_matching_request", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "date": date
+        })
+    });
 
-// セレクトボックスに月の値を与える関数
-function set_months(){
-    const select_box = document.getElementById("month");
-    const months = 12;
+    try {
+        const result = await response.json();
 
-    for(let i = 0; i < months; i++){
-        let select_option = document.createElement("option");
-        select_option.textContent = i + 1;
-        select_option.value = i + 1;
-        select_box.append(select_option);
+        if (!response.ok || !result || result.length <= 0) {
+            create_not_search_text();
+        }
+    } catch (error) {
+        create_server_error_text();
     }
-    
-    return 0;
-}
 
-// セレクトボックスに日にちの値を与える関数
-function set_days(){
-    const select_box = document.getElementById("day");
-    const days = 31;
-
-    for(let i = 0; i < days; i++){
-        let select_option = document.createElement("option");
-        select_option.textContent = i + 1;
-        select_option.value = i + 1;
-        select_box.append(select_option);
-    }
-    
-    return 0;
+    return result;
 }
 
 // 検索ボタンに検索機能を与える関数
@@ -122,14 +118,12 @@ function give_function_search_button(){
 
 // 検索処理を行う機能
 function search_by_date(){
-    const year = document.getElementById("year").value;
-    const month = document.getElementById("month").value;
-    const day = document.getElementById("day").value;
-
+    const date = document.getElementById("date").value;
+    if(date == "") return;
     const list = document.getElementById("list");
     list.innerHTML = "";
 
-    create_kitchen_list(year, month, day, list);
+    create_kitchen_list(date, list);
 
     return 0;
 }
@@ -137,17 +131,23 @@ function search_by_date(){
 // エラー文をクリアする関数
 function clear_error_text(){
     const text_box = document.getElementById("error");
-    text_box.className = "COMMON_TEXT";
     text_box.innerHTML = "";
 
     return 0;
 }
 
-// エラー文を表示する関数
-function create_error_text(){
+// 検索結果が見つからなかった時のエラー文を表示する関数
+function create_not_search_text(){
     const text_box = document.getElementById("error");
-    text_box.className = "COMMON_TEXT";
     text_box.textContent = "検索結果が見つかりませんでした";
+
+    return 0;
+}
+
+// データベースとの通信でエラーが起こった場合のエラー文を表示する関数
+function create_server_error_text(){
+    const text_box = document.getElementById("error");
+    text_box.textContent = "サーバーエラーが起こりました";
 
     return 0;
 }
