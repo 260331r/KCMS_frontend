a511c551207d560a80212dcf7b9adaab85b59646
260331r <147483520+260331r@users.noreply.github.com>
2025-01-02 17:11:20 +0900

    Update schedule_list.js

--- kitchen/schedule_list.js
+++ kitchen/schedule_list.js
@@ -5,24 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // 本日以降のすべての日時のリストを作成する関数
 function init_create_schedule_list(){
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-
-    const all_elements = db_all_elements(year, month, day);
+    const user_id = localStorage.getItem("user_id");
+    const all_elements = db_all_elements(user_id);
     const list = document.getElementById("list");
 
-    create_place_frame_list(all_elements, list);
-    if(all_elements.length <= 0){
-        create_error_text();
-    }
+    create_shop_frame_list(all_elements, list);
 
     return 0;
 }
 
 // リストの枠を量産する関数
-function create_place_frame_list(place_array, list){
+function create_shop_frame_list(place_array, list){
     for(let i = 0; i < place_array.length; i++){
         const frame = make_frame(place_array[i]);
         list.append(frame);
@@ -38,35 +31,59 @@ function make_frame(place_details){
 
     const place_schedule = document.createElement("p");
     place_schedule.className = "SCHEDULE_TEXT";
-    place_schedule.textContent = "出店予定日時 : " + place_details[2];
+    place_schedule.textContent = "出店予定日時 : " + place_details.日時;
     frame.append(place_schedule);
 
     const place_name = document.createElement("p");
     place_name.className = "NAME_TEXT";
-    place_name.textContent = place_details[0];
+    place_name.textContent = place_details.場所名;
     frame.append(place_name);
 
-    const place_address = document.createElement("p");
-    place_address.className = "ADDRESS_TEXT";
-    place_address.textContent = place_details[1];
-    frame.append(place_address);
+    const place_genre = document.createElement("p");
+    place_genre.className = "ADRESS_TEXT";
+    place_genre.textContent = place_details.住所;
+    frame.append(place_genre);
 
     return frame;
 }
 
-// データベースから本日以降のすべての提供予定の要素を取得する関数
-// [[場所名, 住所, 掲示日時], ...]のような形式
-// スケジュールテーブルから、日時がnullでないものかつ、出店者IDが自身と一致しているものを取り出す
-function db_all_elements(year, month, day){
-    // テストのため適当に配列を作っているだけ
-    return [["おにぎり屋", "高知県香美市土佐山田町1234-56", "2024/3/4"], [5,5,5]];
+// データベースから本日以降のすべての出店予定の要素を取得する関数
+async function db_all_elements(user_id){
+    const response = await fetch("http://127.0.0.1:8000/api/store/store_schedule_check/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "user_id": user_id
+        })
+    });
+
+    try {
+        const result = await response.json();
+
+        if (!response.ok || !result || result.length <= 0) {
+            create_not_search_text();
+        }
+    } catch (error) {
+        create_server_error_text();
+    }
+
+    return result;
 }
 
-// エラー文を表示する関数
-function create_error_text(){
+// 検索結果が見つからなかった時のエラー文を表示する関数
+function create_not_search_text(){
     const text_box = document.getElementById("error");
-    text_box.className = "COMMON_TEXT";
     text_box.textContent = "検索結果が見つかりませんでした";
+
+    return 0;
+}
+
+// データベースとの通信でエラーが起こった場合のエラー文を表示する関数
+function create_server_error_text(){
+    const text_box = document.getElementById("error");
+    text_box.textContent = "サーバーエラーが起こりました";
 
     return 0;
 }
