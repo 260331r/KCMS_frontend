a599c2edb9740700264edfbb5dcc60642991e32e
hisada <147483849+1260354@users.noreply.github.com>
2025-01-10 10:53:17 +0900

    js仮ファイル書き込み

--- manager/account_view.js
+++ manager/account_view.js
@@ -2,7 +2,7 @@ document.getElementById('deleteButton').addEventListener('click', function() {
     // 削除確認のポップアップ
     if (confirm('本当に削除しますか？')) {
         // 「はい」が選択された場合、削除しました画面に遷移
-        window.location.href = 'acount_delete.html';
+        window.location.href = 'account_delete.html';
     } else {
         // 「いいえ」が選択された場合、何もせず遷移しない
         return;
