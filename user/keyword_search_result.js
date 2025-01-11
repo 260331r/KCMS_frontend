55e2f795f1b4ec3b537b3f1192f4790ea737f74c
Shun260334 <260334k@ugs.kochi-tech.ac.jp>
2025-01-11 09:18:52 +0000

    キーワード検索結果画面修正

--- user/keyword_search_result.js
+++ user/keyword_search_result.js
@@ -1,35 +1,84 @@
-// ダミーの店舗名を作成.
-// それぞれ遷移先のリンクが必要であるが，今回は省く.
-const storedata = [
-    {name: "ダミー店舗名１"},
-    {name: "ダミー店舗名２"},
-    {name: "ダミー店舗名３"},
-    {name: "ダミー店舗名４"}
-]; 
+document.addEventListener("DOMContentLoaded", () => {
+    fetch_before();
+})
 
-const resultlist = document.querySelector('.RESULT_LIST');
+function fetch_before() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const encodedData = urlParams.get('param');
+    console.log(encodedData);
+    if (encodedData) {
+        try{
+            const searched_elements = JSON.parse(decodeURIComponent(encodedData));
+            console.log(searched_elements);
+            store_view_result(searched_elements);
+            place_view_result(searched_elements);
+        } catch (error) {
+            console.error('データ解析中にエラーが発生しました');
+        }
+        
+    }
+}
 
-storedata.forEach(store => {
-    // 各店舗のコンテナを作成する
-    const resultcontainer = document.createElement('div');
-    resultcontainer.className = 'RESULT_CONTAINER';
+function store_view_result(searched_elements) {
+    const resultlist = document.querySelector('#STORE_VIEW');
+    let count = 0;
+    for (let i = 0; i < searched_elements.length; i++) {
+        if (searched_elements[i].出店者名 == null) continue;
+        const resultcontainer = document.createElement('div');
+        resultcontainer.className = 'RESULT_CONTAINER';
 
-    // 店舗名書き込み
-    const storeName = document.createElement('p');
-    storeName.className = 'COMMON_TEXT';
-    // 全部色が#ff7f00だとメリハリがつかない気がする．
-    storeName.style = 'color: black'; 
-    storeName.textContent = store.name;
+        const storeName = document.createElement('p');
+        storeName.className = 'COMMON_TEXT STORE_NAME_TEXT';
+        console.log(searched_elements[i].出店者名);
+        storeName.textContent = searched_elements[i].出店者名;
+        resultcontainer.appendChild(storeName);
 
-    // 各コンテナのボタン
-    const detailbutton = document.createElement('button');
-    detailbutton.className = 'COMMON_BUTTON COMMON_BUTTON_LARGE';
-    detailbutton.textContent = '詳しくはこちら';
+        const detailButton = document.createElement('button');
+        detailButton.className = 'COMMON_BUTTON COMMON_BUTTON_LARGE';
+        detailButton.textContent = "詳しくはこちら";
+        detailButton.addEventListener('click', function() {
+            // 出店者IDを詳細画面に渡し，ページ遷移
+        })
+        resultcontainer.appendChild(detailButton);
 
-    // コンテナに要素追加
-    resultcontainer.appendChild(storeName);
-    resultcontainer.appendChild(detailbutton);
+        resultlist.appendChild(resultcontainer);
+        count++;
+    }
+    create_no_search_text(resultlist, count);
+}
 
-    // 親要素に追加
-    resultlist.appendChild(resultcontainer);
-});
+function place_view_result(searched_elements) {
+    const resultlist = document.querySelector('#PLACE_VIEW');
+    let count = 0;
+    for (let i = 0; i < searched_elements.length; i++) {
+        if (searched_elements[i].場所名 == null) continue;
+        const resultcontainer = document.createElement('div');
+        resultcontainer.className = 'RESULT_CONTAINER';
+
+        const placeName = document.createElement('p');
+        placeName.className = 'COMMON_TEXT STORE_NAME_TEXT';
+        console.log(searched_elements[i].場所名);
+        placeName.textContent = searched_elements[i].場所名;
+        resultcontainer.appendChild(placeName);
+
+        const detailButton = document.createElement('button');
+        detailButton.className = 'COMMON_BUTTON';
+        detailButton.textContent = "詳しくはこちら";
+        detailButton.addEventListener('click', function() {
+            // 場所IDを詳細画面に渡し，ページ遷移
+        })
+        resultlist.appendChild(resultcontainer);
+        count++;
+    }
+    create_no_search_text(resultlist, count);
+}
+
+function create_no_search_text(parent_element, count) {
+    if (count === 0){
+        const textbox = document.createElement('p');
+        textbox.className = 'COMMON_TEXT';
+        textbox.style = 'color: black';
+        textbox.textContent = '合致するものが見つかりませんでした';
+        parent_element.appendChild(textbox);
+    }
+}
