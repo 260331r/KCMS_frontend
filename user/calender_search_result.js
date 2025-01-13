d2e1f7e144865ef915efcbc85e57ad6d3bbfed62
Shun260334 <260334k@ugs.kochi-tech.ac.jp>
2025-01-13 03:17:12 +0000

    コーディング規約に合うよう修正

--- user/calender_search_result.js
+++ user/calender_search_result.js
@@ -3,48 +3,46 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 function fetch_before() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const encodedData = urlParams.get('param');
-    console.log(encodedData);
-    if (encodedData) {
-        const searched_elements = JSON.parse(decodeURIComponent(encodedData));
-        console.log(searched_elements);
+    const url_params = new URLSearchParams(window.location.search);
+    const encoded_data = url_params.get('param');
+    if (encoded_data) {
+        const searched_elements = JSON.parse(decodeURIComponent(encoded_data));
         view_result(searched_elements);
     }
 }
 
 // 検索結果を表示させる．
 function view_result(searched_elements) {
-    const resultlist = document.querySelector('.RESULT_LIST');
+    const result_list = document.querySelector('.RESULT_LIST');
     for (let i = 0; i < searched_elements.length; i++) {
-        const resultcontainer = document.createElement('div');
-        resultcontainer.className = 'RESULT_CONTAINER';
+        const result_container = document.createElement('div');
+        result_container.className = 'RESULT_CONTAINER';
 
-        const storeName = document.createElement('p');
-        storeName.className = 'COMMON_TEXT STORE_NAME_TEXT';
+        const store_name = document.createElement('p');
+        store_name.className = 'COMMON_TEXT STORE_NAME_TEXT';
         console.log(searched_elements[i].出店者名);
-        storeName.textContent = searched_elements[i].出店者名;
-        resultcontainer.appendChild(storeName);
+        store_name.textContent = searched_elements[i].出店者名;
+        result_container.appendChild(store_name);
 
         const genre = document.createElement('p');
         genre.className = 'COMMON_TEXT GENRE_TEXT';
         genre.style = 'color: black';
         genre.textContent = "商品ジャンル: " + searched_elements[i].商品ジャンル;
-        resultcontainer.appendChild(genre);
+        result_container.appendChild(genre);
 
-        const placeName = document.createElement('p');
-        placeName.className = "COMMON_TEXT PLACE_NAME_TEXT";
-        placeName.style = 'color: black';
-        placeName.textContent = "開催場所: " + searched_elements[i].場所名;
-        resultcontainer.appendChild(placeName);
+        const place_name = document.createElement('p');
+        place_name.className = "COMMON_TEXT PLACE_NAME_TEXT";
+        place_name.style = 'color: black';
+        place_name.textContent = "開催場所: " + searched_elements[i].場所名;
+        result_container.appendChild(place_name);
 
 
         const address = document.createElement('p');
         address.textContent = "住所: " + searched_elements[i].住所
         address.style = 'color: black';
-        resultcontainer.appendChild(address);
+        result_container.appendChild(address);
 
 
-        resultlist.appendChild(resultcontainer);
+        result_list.appendChild(result_container);
     }
 }
